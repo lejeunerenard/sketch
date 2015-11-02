48b272bfc1fba84affe497c30958401a57bfc5a2
Sean Zellmer <sean@lejeunerenard.com>
2015-11-01 22:07:41 -0600

    Create linear gradient between two colors for triangles' fill
    Playing with the idea of interpolating between two colors in js.
    
    Broke down the color into it's RGB components, took the difference
    between each component of the two color, multiplied it by the value
    between 0 to 1 and added it to the component of the first color.
    Finally covert back to hex.
    
    One important detail was the flooring of the components decimal number
    before converting to hex. This prevented it from changing the columns
    of the other columns. IE, red's decimal place couldn't change the value
    of green or blue.

--- experiments/geometrics/src/app.js
+++ experiments/geometrics/src/app.js
@@ -56,11 +56,11 @@ class App {
           triangleHeight = -this.sideLength;
 
       let colors = [
-         '#556270',
-         '#4ECDC4',
-         '#C7F464',
-         '#FF6B6B',
-         '#C44D58',
+         '556270',
+         '4ECDC4',
+         'C7F464',
+         'FF6B6B',
+         'C44D58',
       ];
 
       while ( triangleHeight < this.height ) {
@@ -69,14 +69,15 @@ class App {
             let offset = this.sideLength / 2 * toggle;
             toggle = !toggle;
 
+            var color = this.linerColorInterpolation(colors[1], colors[3], triangleWidth / this.width);
+            console.log('color', color);
+
             // Create triangle pointing right
             this.triangles.push(new Triangle({
                x: triangleWidth,
                y: triangleHeight + offset,
                sideLength: this.sideLength,
-               fillStyle: colors[
-                  Math.floor( Math.random() * colors.length )
-               ],
+               fillStyle: '#'+color,
             }));
 
             // Create triangle pointing left
@@ -85,9 +86,7 @@ class App {
                y: triangleHeight + ( this.sideLength / 2 ) + offset,
                sideLength: this.sideLength,
                rotate: 180,
-               fillStyle: colors[
-                  Math.floor( Math.random() * colors.length )
-               ],
+               fillStyle: '#'+color,
             }));
 
             // H = B/2
@@ -97,6 +96,37 @@ class App {
          triangleHeight += this.sideLength;
          triangleWidth = 0;
       }
+   }
+
+   linerColorInterpolation( firstColor, secondColor, percentage ) {
+      var rgb1 = this.hexToRgb(firstColor);
+      var rgb2 = this.hexToRgb(secondColor);
+
+      return Math.floor(
+      // Red
+       Math.floor( ( rgb2.r - rgb1.r ) * percentage + rgb1.r ) * 0x010000 +
+      // Green
+       Math.floor( ( rgb2.g - rgb1.g ) * percentage + rgb1.g ) * 0x000100 +
+      // Blue
+       Math.floor( ( rgb2.b - rgb1.b ) * percentage + rgb1.b ) * 0x000001 
+      ).toString(16);
+   }
+
+   // Source:
+   // http://stackoverflow.com/a/5624139/630490
+   hexToRgb(hex) {
+       // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+       var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+       hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+           return r + r + g + g + b + b;
+       });
+
+       var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+       return result ? {
+           r: parseInt(result[1], 16),
+           g: parseInt(result[2], 16),
+           b: parseInt(result[3], 16)
+       } : null;
    }
 }
 
