1bc9c8af698b7589af39d49fee93bc091dbe79f6
Sean Zellmer <sean@lejeunerenard.com>
2016-05-29 18:20:09 -0500

    obscured-objects: feat: Split out capture js & conditionally load it
    This is an optimization for non-capture viewing.

--- experiments/obscured-objects/index.js
+++ experiments/obscured-objects/index.js
@@ -1,43 +1,15 @@
 import createLoop from 'canvas-fit-loop'
-import fitter from 'canvas-fit'
-import CCapture from 'ccapture.js'
-import defined from 'defined'
 import App from './app'
 
 let app = new App()
+window.app = app
 
 let capture = (localStorage.capture !== 'false' && localStorage.capture)
-let captureLength = defined(localStorage.captureLength || 10)
 
 if (capture) {
-  fitter(app.canvas)
-
-  let capturer
-  let framerate = 30
-  capturer = new CCapture({ format: 'jpg', framerate, verbose: true })
-  capturer.start()
-
-  let frames = 0
-  let dt = 1 / framerate * 1000
-
-  // Helpers
-  let render = function render () {
-    requestAnimationFrame(render)
-
-    app.update(dt)
-    app.render()
-
-    if (frames <= captureLength * 30) {
-      capturer.capture(app.canvas)
-      frames++
-    } else {
-      capturer.stop()
-      capturer.save()
-    }
-  }
-
-  app.resize()
-  render()
+  let captureScript = document.createElement('script')
+  document.body.appendChild(captureScript)
+  captureScript.src = 'bundle.capture.js'
 } else {
   const loop = createLoop(app.canvas, {
     scale: window.devicePixelRatio
