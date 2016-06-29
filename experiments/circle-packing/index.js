de0ba9c104f41dd21ed3acc1e3da27bf2d9cba68
Sean Zellmer <sean@lejeunerenard.com>
2016-06-29 13:53:25 -0500

    circle-packing: Add voyeur to capture

--- experiments/circle-packing/index.js
+++ experiments/circle-packing/index.js
@@ -1,7 +1,15 @@
+let Voyeur = require('@lejeunerenard/voyeur')
 import App from './app'
 
 let app = new App()
 document.body.appendChild(app.canvas)
-app.start()
+
+let voyeur = new Voyeur(app.canvas, 5, {
+  framerate: 10
+})
+voyeur.capture = localStorage.capture
+voyeur.update = app.update.bind(app)
+voyeur.render = app.render.bind(app)
+voyeur.start()
 
 window.app = app
